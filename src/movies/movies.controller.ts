b23641c37b7c4efa8b207949780d51d6971cb6c1
Taeyang-Choi <zhfldk1027@gmail.com>
2022-01-22 16:22:13 +0900

    2.4 DTOs and validation

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -10,6 +10,8 @@ import {
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie-dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -21,22 +23,22 @@ export class MoviesController {
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string): Movie {
+  getOne(@Param('id') id: number): Movie {
     return this.moviesServie.getOne(id);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesServie.create(movieData);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: number) {
     return this.moviesServie.deleteOne(id);
   }
 
   @Patch(':id')
-  path(@Param('id') id: string, @Body() updateData) {
+  path(@Param('id') id: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesServie.update(id, updateData);
   }
 }
